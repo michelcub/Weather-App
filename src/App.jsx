4c715a743b9792635cbc3053dc4a2a71fe05d75c
michelcub <orestelopez96@gmail.com>
2023-06-19 22:27:09 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App(){
   
   if(!data?.hasOwnProperty('main')){
     return (
-      <section className='overflow-hidden bg-[url("./src/assets/images/soleado.jpg")] bg-cover bg-top bg-no-repeat w-sreen h-screen flex items-center justify-center flex-col gap-3 relative'>
+      <section className='overflow-hidden bg-[url("./assets/images/soleado.jpg")] bg-cover bg-top bg-no-repeat w-sreen h-screen flex items-center justify-center flex-col gap-3 relative'>
         <h1 className="p-4 text-4xl text-white bg-gray-300 rounded-xl">Loading...</h1>
       </section>
     )
