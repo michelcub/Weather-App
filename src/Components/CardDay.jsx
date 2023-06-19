182fc10cdb2b8575f26de689cb2e03a5f61128c2
michelcub <orestelopez96@gmail.com>
2023-06-19 22:32:28 +0100

    fix card day

--- src/Components/CardDay.jsx
+++ src/Components/CardDay.jsx
@@ -2,7 +2,7 @@
 
 export const CardDay = ({temp, time}) => {
     return (
-        <article className="flex flex-col items-center p-4 border-[2px] hover:border-orange-400 rounded-[5rem]">
+        <article className="flex flex-col items-center p-4 border-[2px] hover:border-orange-400 rounded-[7rem]">
               <p className="text-gray-300">{time}</p>
               <img className="w-[2.5rem]" src=".././assets/icons/Property_1=Sun.svg" alt="sun" />
               <div className="flex text-gray-200">
